70e5f18d5d8afc5337c231e165aa68352bd2d394
Alan <Alan@gmail>
2024-11-05 19:36:53 -0300

    Atualização do arquivo Banco e Server

--- banco.js
+++ banco.js
@@ -1,60 +1,63 @@
 const sqlite = require('sqlite')
 const sqlite3 = require('sqlite3')
 
-class Banco {
+
+class Banco{
 
     constructor(){
-        criarTabela()
+        this.criarTabela()
     }
+    
 
     async sqlConnection() {
-        const banco = await sqlite.open({
-            filename: 'database.db',
-            driver: sqlite3.Database
-        })
-    
-        return banco;
-    
+    const banco = await sqlite.open({
+        filename: 'database.db',
+        driver: sqlite3.Database
+    })
+
+    return banco;
+
     }
-    
+
     async criarTabela() {
-        const banco = await sqlConnection();
-    
-        const tabela = `CREATE TABLE IF NOT EXISTS alunos (
-                        id integer PRIMARY KEY AUTOINCREMENT,
-                        uuid varchar(100),
-                        nome varchar(100),
-                        email varchar(100)
-                        );`;
-    
-        await banco.exec(tabela) 
+    const banco = await this.sqlConnection();
+
+    const tabela = `CREATE TABLE IF NOT EXISTS alunos (
+                    id integer PRIMARY KEY AUTOINCREMENT,
+                    uuid varchar(100),
+                    nome varchar(100),
+                    email varchar(100)
+                    );`;
+
+    await banco.exec(tabela) 
     }
-    
+
     async inserir(aluno) {
-        const {uuid, nome, email} = aluno;
-        const banco = await sqlConnection();
-        await banco.run("INSERT INTO alunos (uuid, nome, email) values (?, ?, ?)", uuid, nome, email)
+    const {uuid, nome, email} = aluno;
+    const banco = await this.sqlConnection();
+    await banco.run("INSERT INTO alunos (uuid, nome, email) values (?, ?, ?)", uuid, nome, email)
     }
-    
+
     async remover(id) {
-        const banco = await sqlConnection();
-        await banco.run("DELETE FROM alunos WHERE id = ?", id)
+    const banco = await this.sqlConnection();
+    await banco.run("DELETE FROM alunos WHERE id = ?", id)
     }
-    
+
     async atualizar(aluno) {
-        const {uuid, nome, email, id} = aluno;
-        const banco = await sqlConnection();
-        await banco.run("UPDATE alunos SET uuid = ?, nome = ?, email = ? WHERE id = ?", uuid, nome, email, id )
+    const {uuid, nome, email, id} = aluno;
+    const banco = await this.sqlConnection();
+    await banco.run("UPDATE alunos SET uuid = ?, nome = ?, email = ? WHERE id = ?", uuid, nome, email, id )
     }
-    
+
     async listar() {
-        const banco = await sqlConnection();
-        const result = await banco.all("SELECT * FROM alunos")
-        console.log(result)
-    
-        return result
+    const banco = await this.sqlConnection();
+    const result = await banco.all("SELECT * FROM alunos")
+    console.log(result)
+
+    return result
     }
-    
+
 }
+
 
 module.exports = Banco;
